be19788b1d830b02704b32d5078cbca4685fa045
PingTouG <2798874773@qq.com>
2021-08-19 23:29:25 +0800

    chore: index.html标题根据配置文件自动设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,36 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import type { UserConfigExport, ConfigEnv } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { injectHtml } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
-  resolve: {
-    alias: [
-      // 处理通过"~"引入Antd问题
-      { find: /^~/, replacement: '' },
-      { find: '@', replacement: '/src' },
+export default ({ mode }: ConfigEnv): UserConfigExport => {
+  return defineConfig({
+    plugins: [
+      reactRefresh(),
+      injectHtml({
+        injectData: {
+          title: loadEnv(mode, process.cwd()).VITE_APP_NAME,
+        },
+      }),
     ],
-  },
-  // 处理引入Antd样式报错问题
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
+    resolve: {
+      alias: [
+        // 处理通过"~"引入Antd问题
+        { find: /^~/, replacement: '' },
+        { find: '@', replacement: '/src' },
+      ],
+    },
+    // 处理引入Antd样式报错问题
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
+      },
+      modules: {
+        localsConvention: 'camelCaseOnly',
       },
     },
-  },
-})
+  })
+}
